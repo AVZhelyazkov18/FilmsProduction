db8b547aed85424bfd8a7a640d16cce8030cedd5
N3ONATIC <95431924+N3ONATIC@users.noreply.github.com>
2022-10-24 11:39:03 +0200

    Commit 2

--- demo/src/main/java/com/example/demo/ContentBasedFilter.java
+++ demo/src/main/java/com/example/demo/ContentBasedFilter.java
@@ -1,5 +1,8 @@
 package com.example.demo;
 
+import org.springframework.stereotype.Component;
+
+@Component
 public class ContentBasedFilter implements Filter {
     public String[] getRecommendations(String movie) {
         return new String[] {"Happy Feet", "Ice Age", "Shark Tale"};
--- demo/src/main/java/com/example/demo/RecommenderImplementation.java
+++ demo/src/main/java/com/example/demo/RecommenderImplementation.java
@@ -1,7 +1,11 @@
 package com.example.demo;
 
-public class RecommenderImplementation {
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
 
+@Component
+public class RecommenderImplementation {
+    @Autowired
     private Filter filter;
     public String[] recommendedMovies(String movie) {
         return new String[] {"M1", "M2", "M3"};
