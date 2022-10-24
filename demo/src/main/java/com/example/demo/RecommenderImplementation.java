b978379c3435558ab9028134c231e700c095bd55
N3ONATIC <95431924+N3ONATIC@users.noreply.github.com>
2022-10-24 10:47:52 +0200

    Initial commit

---
+++ demo/src/main/java/com/example/demo/RecommenderImplementation.java
@@ -0,0 +1,7 @@
+package com.example.demo;
+
+public class RecommenderImplementation {
+    public String[] recommendedMovies(String movie) {
+        return new String[] {"M1", "M2", "M3"};
+    }
+}
---
+++ demo/src/main/java/com/example/demo/RecommenderImplementationApplication.java
@@ -0,0 +1,17 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.util.Arrays;
+
+@SpringBootApplication
+public class RecommenderImplementationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RecommenderImplementationApplication.class, args);
+		RecommenderImplementation recommender = new RecommenderImplementation();
+		String[] result = recommender.recommendedMovies("Finding Dorry");
+		System.out.println(Arrays.toString(result));
+	}
+}
